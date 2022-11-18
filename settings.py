ea4b58164a26b13a015a264104f86e5fd72708c4
ferleyrincon <heiner.rincon@urosario.edu.co>
2022-11-17 22:09:55 -0500

    Comentario

--- settings.py
+++ settings.py
@@ -2,12 +2,12 @@ from os import environ
 import importlib.util
 
 
-if environ.get('OTREE_PRODUCTION') not in {None, '', '0'}:
-    DEBUG = False
-    APPS_DEBUG = False
-else:
-    DEBUG = True
-    APPS_DEBUG = True   # also enables random fill in of forms
+#if environ.get('OTREE_PRODUCTION') not in {None, '', '0'}:
+#    DEBUG = False
+#    APPS_DEBUG = False
+#else:
+#    DEBUG = True
+#    APPS_DEBUG = True   # also enables random fill in of forms
 
 
 # if you set a property in SESSION_CONFIG_DEFAULTS, it will be inherited by all configs
@@ -15,11 +15,9 @@ else:
 # the session config can be accessed from methods in your apps as self.session.config,
 # e.g. self.session.config['participation_fee']
 
-SESSION_CONFIG_DEFAULTS = {
-    'real_world_currency_per_point': 1.00,
-    'participation_fee': 0.00,
-    'doc': "",
-}
+SESSION_CONFIG_DEFAULTS = dict(
+    real_world_currency_per_point=1.00, participation_fee=0.00, doc=""
+)
 
 SESSION_CONFIGS = [
 #    dict(
@@ -28,24 +26,24 @@ SESSION_CONFIGS = [
 #       num_demo_participants=1,
 #       app_sequence=['iat','survey' ]
 #    ),
-    dict(
-       name='pruebas',
-       display_name="pruebas",
-       num_demo_participants=1,
-       app_sequence=['survey']
-    ),
+#    dict(
+#       name='pruebas',
+#       display_name="pruebas",
+#       num_demo_participants=1,
+#       app_sequence=['survey']
+#    ),
 ]
 
-BROWSER_COMMAND = '/usr/bin/chromium-browser'
+#BROWSER_COMMAND = '/usr/bin/chromium-browser'
 
-ROOM_DEFAULTS = {}
+#ROOM_DEFAULTS = {}
 
-ROOMS = [
-    dict(
-        name='PC',
-        display_name='PC',
-        participant_label_file='_rooms/PC.txt',
-    ),
+#ROOMS = [
+#    dict(
+#        name='PC',
+#        display_name='PC',
+#        participant_label_file='_rooms/PC.txt',
+#    ),
 #        dict(
 #        name='PC2',
 #        display_name='PC2',
@@ -56,12 +54,12 @@ ROOMS = [
 #        display_name='PC3',
 #        participant_label_file='_rooms/PC3.txt',
 #    ),
-]
+#]
 
 # ISO-639 code
 # for example: de, fr, ja, ko, zh-hans
 LANGUAGE_CODE = 'es'
-TIME_ZONE = 'UTC'
+#TIME_ZONE = 'UTC'
 
 # e.g. EUR, GBP, CNY, JPY
 REAL_WORLD_CURRENCY_CODE = 'COP'
